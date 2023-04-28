31e18eae3e2de6f87bd62550f0deef4ca36bf5c4
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2023-04-28 18:59:31 -0300

    update front

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import App from './App';
  import axios from 'axios';
 
  //axios.defaults.baseURL= 'http://localhost:3001';
- axios.defaults.baseURL= 'https://backend-qog4.onrender.com/';
- export const ruthApp='https://backend-qog4.onrender.com/';
+ axios.defaults.baseURL= 'https://backend-qog4.onrender.com';
+ export const ruthApp='https://backend-qog4.onrender.com';
 
 
 ReactDOM.render(
