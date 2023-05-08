43f9103578411c99ce6ff5878b337f66e951fdca
Eze323 <a.Ezequielirace@gmail.com>
2023-05-08 05:03:53 -0300

    update form

--- src/components/FormCreate/validation.js
+++ src/components/FormCreate/validation.js
@@ -1,4 +1,4 @@
-const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+//const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 const regex = /^[A-Za-z\s]+$/; // Expresión regular para validar que solo haya letras y espacios
 
 
